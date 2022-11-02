cb91dfbd5466d8d934e05c0cd231bc4e0eb904f6
AYasser <ahmed.yasser.asu@gmail.com>
2022-11-02 17:13:54 +0200

    use ngFor in events-list.component.ts

--- src/app/events/events-thumbnail.component.ts
+++ src/app/events/events-thumbnail.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from "@angular/core";
+import {Component, Input} from '@angular/core';
 
 @Component({
   template: `
@@ -22,6 +22,10 @@ import {Component, Input} from "@angular/core";
 
     .well div {
       color: #bbb;
+    }
+
+    .thumbnail {
+      min-height: 210px;
     }
   `]
 })
