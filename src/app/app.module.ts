e8742836a6328f79bdb42c93f75c069459a6f308
AYasser <ahmed.yasser.asu@gmail.com>
2022-11-02 16:17:04 +0200

    create and navbar.component.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { EventsAppComponent } from './events-app.component';
-import {EventsListComponent} from "./events/events-list.component";
-import {EventsThumbnailComponent} from "./events/events-thumbnail.component";
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {EventsAppComponent} from './events-app.component';
+import {EventsListComponent} from './events/events-list.component';
+import {EventsThumbnailComponent} from './events/events-thumbnail.component';
+import {NavBarComponent} from './nav/navbar.component';
 
 @NgModule({
   imports: [
@@ -12,8 +12,10 @@ import {EventsThumbnailComponent} from "./events/events-thumbnail.component";
   declarations: [
     EventsAppComponent,
     EventsListComponent,
-    EventsThumbnailComponent
+    EventsThumbnailComponent,
+    NavBarComponent
   ],
   bootstrap: [EventsAppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
